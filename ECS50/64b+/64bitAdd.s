7d5b9ad047f1c52cd073f44ad5379f7544b8c3ea
NRuss-1 <nicholasa.russ@gmail.com>
2021-07-01 23:28:02 -0700

    updates

--- ECS50/64b+/64bitAdd.s
+++ ECS50/64b+/64bitAdd.s
@@ -1,29 +1,31 @@
 .global _start
+.equ var, 5
 
 .data
 num1:
-  .long 5
-  .long 10
+    .long 5
+    .long 10
 
 num2:
-  .long 15
-  .long 20
+    .long 15
+    .long 20
 
 .text
-
 _start:
-  #upper 32 bits in EDX
-  #lower 32 bits in EAX
-  movl num1, %edx
-  movl num1+4, %eax
-  addl num2+4, %eax
-  jc carry
-  addl num2, %edx
-  addl num2, %edx
-  jmp done
+    #upper 32 bits in EDX
+    #lower 32 bits in EAX
+
+    movl num1, %edx
+    movl num1+4, %eax
+    addl num2+4, %eax
+
+    jc carry
+    addl num2, %edx
+    jmp done
 
 carry:
-  addl $1, %edx
+    addl num2, %edx
+    addl $1, %edx
 
 done:
-  movl %eax, %eax
+    movl %eax, %eax
